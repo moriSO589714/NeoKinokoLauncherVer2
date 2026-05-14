eaaf78f1838726508da0d720d654ed3ba9dff7ba
nametakeman <132148860+nametakeman@users.noreply.github.com>
2026-05-14 14:23:25 +0900

    [Add]UIアニメーションのスクリプト作成

---
+++ Assets/Scripts/UI/MainUI/ManageMainUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageMainUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _wifiMrkObj;
+    void Init()
+    {
+        //_wifiMrkObj.GetComponent<SimpleUI>()?.PointerEnterAct = 
+    }
+}
---
+++ Assets/Scripts/UI/MoveAnim/SimpleDownAndUp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// DoTweenを用いたオブジェクトの上昇/下降と初期位置へ戻るアニメーション
+/// </summary>
+public class SimpleDownAndUp : MonoBehaviour
+{
+
+}
